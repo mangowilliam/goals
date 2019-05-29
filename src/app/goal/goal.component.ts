e9d8bee51a4d06d6372890cc9fe34f5c069fa3fb
williammango <williammango2015@gmail.com>
2019-05-29 14:28:28 +0300

    add goal project

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
-import { Goal } from './goal'
+import { Component, OnInit } from '@angular/core';
+import { Goal } from '../goal'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+  selector: 'app-goal',
+  templateUrl: './goal.component.html',
+  styleUrls: ['./goal.component.css']
 })
-export class AppComponent {
+export class GoalComponent implements OnInit {
+
   goals = [
     new Goal(1, 'Watch Finding Nemo', 'Find an online version and watch merlin find his son', new Date(2018, 3, 14)),
     new Goal(2, 'Buy Cookies', 'I have to buy cookies for the parrot', new Date(2018, 6, 9)),
@@ -15,4 +17,18 @@ export class AppComponent {
     new Goal(6, 'Plot my world domination plan', 'Cause I am an evil overlord', new Date(2018, 3, 14)),
 
   ]
+
+  completeGoal(isComplete, index) {
+    if (isComplete) {
+      this.goals.splice(index, 1);
+    }
+  }
+
+  toogleDetails(index) {
+    this.goals[index].showDescription = !this.goals[index].showDescription;
+  }
+
+  constructor() { }
+  ngOnInit() {
+  }
 }
