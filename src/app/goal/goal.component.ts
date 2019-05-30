328305c37a712dbdc8f7dfc69e9db92c0f1f690a
williammango <williammango2015@gmail.com>
2019-05-30 08:19:06 +0300

    add delete function

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -18,12 +18,15 @@ export class GoalComponent implements OnInit {
 
   ]
 
-  completeGoal(isComplete, index) {
+  deleteGoal(isComplete, index) {
     if (isComplete) {
-      this.goals.splice(index, 1);
+      let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}`)
+
+      if (toDelete) {
+        this.goals.splice(index, 1)
+      }
     }
   }
-
   toogleDetails(index) {
     this.goals[index].showDescription = !this.goals[index].showDescription;
   }
